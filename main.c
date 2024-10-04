ff5b21d48556e820962225d139f32e9394dfd248
Brian Sindihebura <briansindi4@gmail.com>
2024-10-04 13:41:50 +0200

    Add status variable in main() in main branch

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    printf("Welcome to the hotfix branch!\n");
+    printf("Welcome to the main branch!\n");
     printf("This is a new line!\n");
     printf("Another change in the main.c file.\n");
     return 0;
